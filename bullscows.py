2be578d332c2199486b193cbdc524e92962bad6c
vadim <vadim.tikanov@gmail.com>
2017-07-03 16:37:28 +0300

    doc block formatting

--- bullscows.py
+++ bullscows.py
@@ -1,21 +1,27 @@
-"""
-Bulls and Cows game: https://en.wikipedia.org/wiki/Bulls_and_Cows
-"""
+"""Bulls and Cows game: https://en.wikipedia.org/wiki/Bulls_and_Cows."""
 from itertools import permutations
 
 
 class Bullscows:
+    """Implements Bulls and Cows game."""
+
     def __init__(self, alphabet, secret_len):
+        """Set up game context.
+
+        :param alphabet: String, containing possible symbols used in secret.
+        :param secret_len: Secret length.
+        :return:
+        """
         self.alphabet = alphabet
         self.secret_len = secret_len
         self.all_possible_options = [''.join(c) for c in permutations(self.alphabet, self.secret_len)]
         self.remaining_options = list(self.all_possible_options)
-        self.guess = self.alphabet[:self.secret_len]
+        self.guess = self.alphabet[:self.secret_len] #Todo: handle len(alphabet) < len(secret)
 
     @staticmethod
     def evaluate(guess, secret):
-        """
-        Evaluate guess and return (cows, bulls) tuple.
+        """Evaluate guess and return (cows, bulls) tuple.
+
         Cows - count of characters in different position.
         Bulls - count of characters in same position.
             >>> Bullscows.evaluate('5678', '1234')
@@ -31,8 +37,13 @@ class Bullscows:
         bulls = sum(g == s for g, s in zip(guess, secret))
         return cows, bulls
 
+    def verify_guess(self, secret):
+        """Check if guess matches secret."""
+        return self.guess == secret
+
     def adjust_guess(self, result):
-        """
+        """Adjust current guess using simple sieve algorithm.
+
         Reduce remaining options, picking only ones which give same evaluate result if they're the secret.
         Update guess to first remaining option.
         """
--- example.py
+++ example.py
@@ -1,3 +1,4 @@
+"""Example of Bulls and Cows module usage."""
 from bullscows import Bullscows
 import random
 from itertools import count
@@ -5,19 +6,23 @@ from itertools import count
 settings = {
     'alphabet': '0123456789',
     'secret_len': 4
-
 }
 
 secret = ''.join(str(digit) for digit in random.sample(*settings.values()))
 print "Generated secret is: {}".format(secret)
 bc = Bullscows(**settings)
 
-for n in count(1):
+for number_of_tries in count(1):
     print "Remaining options count: {}".format(len(bc.remaining_options))
     print "Guess: {}".format(bc.guess)
     result = Bullscows.evaluate(bc.guess, secret)
     print "Response: {} cows, {} bulls".format(*result)
-    if bc.guess == secret:
-        print "I guessed {}. Number of tries: {}".format(bc.guess, n)
+    if bc.verify_guess(secret):
+        print "I guessed {}. Number of tries: {}".format(bc.guess, number_of_tries)
         break
     bc.adjust_guess(result)
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
